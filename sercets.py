54a4025e13e20f5cc704511aabb4bc9cb11f7a96
Kostevych Viacheslav <tomkat.s@gmail.com>
2026-05-11 10:43:36 +0300

    add secrets 3

--- sercets.py
+++ sercets.py
@@ -1,6 +1,7 @@
 from infisical_sdk import InfisicalSDKClient
 import os
 from dotenv import load_dotenv
+from flask import jsonify
 
 load_dotenv()
 
@@ -14,15 +15,30 @@ client.auth.universal_auth.login(
 )
 
 # # Отримання списку секретів
-# secrets = client.secrets.list_secrets(
-#     project_id=os.getenv("INFISICAL_PROJECT_ID"),
-#     environment_slug="dev",
-#     secret_path="/"
-# )
-#
-# # Вивід результатів
-# for s in secrets.secrets:
-#     print(f"Ключ: {s.secretKey} | Значення: {s.secretValue}")
+def get_secrets_list(env):
+    import json
+    secrets = client.secrets.list_secrets(
+        project_id=os.getenv("INFISICAL_PROJECT_ID"),
+        environment_slug=env,
+        secret_path="/"
+    )
+    secrets_list = secrets.secrets
+    secrets_dict = {s.secretKey: s.secretValue for s in secrets_list}
+
+    # Перетворюємо у формат JSON (рядок)
+    secrets_json = json.dumps(secrets_dict, indent=4)
+    return secrets_json
+    # for s in secrets.secrets:
+    #     print(f" {s.secretKey} : {s.secretValue}")
+
+def get_secrets_dict(env):
+    secrets = client.secrets.list_secrets(
+        project_id=os.getenv("INFISICAL_PROJECT_ID"),
+        environment_slug=env,
+        secret_path="/"
+    )
+    # Повертаємо чистий словник — це найзручніше для Python
+    return {s.secretKey: s.secretValue for s in secrets.secrets}
 
 def get_secret_value(secret_name,env):
     secret = client.secrets.get_secret_by_name(
@@ -36,4 +52,6 @@ def get_secret_value(secret_name,env):
         version=None # Optional
     )
     return secret.secretValue
-print (get_secret_value("budget_sync_id","prod"))
+# print(get_secrets_list("dev"))
+# print (get_secret_value("budget_sync_id","prod"))
+print(get_secrets_dict("dev"))
